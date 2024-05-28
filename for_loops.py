0f0113a3d75dd60d95e7f44a6716d5df034e0878
eyebullsjohnson <bullseyejohnson@gmail.com>
2024-05-28 18:26:33 +0000

    added a for loop demo

---
+++ for_loops.py
@@ -0,0 +1,6 @@
+names = ("Luna","Ginny","benji")
+
+for nombre in names:
+    print(nombre + " is wanted for tax fraud!")
+
+# "nombre is declared in the for statement"
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-password = input("please set a new password")
+valid_Password = False
 
-Special_character_check= "!" in password
+#until we get a valid password
+while not valid_Password:
+    password = input("please set a new password")
 
-if len(password) < 16 and len(password) > 5:
-    print("password is corect lenth")
-elif len(password) < 5:
-    print("Password is too short")
-else:
-    print("password must be between 5 and 16 characters")
+    length_valid=False
+    special_character_valid=False
 
-if Special_character_check:
-    print("special character found")
-else:
-    print("this password needs an ! somewhere")
+    if len(password) < 16 and len(password) > 5:
+        print("password is correct length")
+        length_valid = True
+    elif len(password) < 5:
+        print("Password is too short")
+    else:
+        print("password must be between 5 and 16 characters")
+
+    if "!"  in password:
+        print("special character found")
+        special_character_valid = True
+    else:
+        print("this password needs an ! somewhere")
+
+    if special_character_valid and length_valid:
+            print("your password has been set")
+            valid_Password=True
+    else:
+         print("try again")
+print("good job")
